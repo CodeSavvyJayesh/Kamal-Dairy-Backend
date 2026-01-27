fee3efff32e28c9d282be7852262373fdc99f56f
Jayesh <jayeshdhamal327@gmail.com>
2026-01-27 12:48:50 +0530

    DataJPA annotations added,Entity annotations

--- src/main/java/com/kamaldairy/kamal_dairy_backend/model/Product.java
+++ src/main/java/com/kamaldairy/kamal_dairy_backend/model/Product.java
@@ -1,4 +1,4 @@
-package com.kamaldairy.kamal_dairy_backend.model;
+/*package com.kamaldairy.kamal_dairy_backend.model;
 
 public class Product {
     private int id;
@@ -36,4 +36,78 @@ public class Product {
         return imageUrl;
     }
 
+}
+*/
+
+// now we have to convert this product.java to entity
+
+package com.kamaldairy.kamal_dairy_backend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private double price;
+    private String category;
+    private String imageUrl;
+
+    // required by JPA
+    public Product(){
+
+    }
+    public Product(int id,String name,double price,String category,String imageUrl)
+    {
+         this.id = id;
+         this.name = name;
+         this.price = price;
+         this.category = category;
+         this.imageUrl = imageUrl;
+    }
+
+    // getters and setters
+    public int getId(){
+         return id;
+    }
+    public String getName(){
+         return name;
+
+    }
+    public double getPrice(){
+         return price;
+
+    }
+    public String getCategory(){
+         return category;
+    }
+    public String getImageUrl(){
+        return imageUrl;
+    }
+
+    public void setID(int id)
+    {
+         this.id = id;
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    public void setPrice(double price)
+    {
+        this.price = price;
+
+    }
+    public void setCategory(String category)
+    {
+        this.category = category;
+
+    }
+    public void setImageUrl(String imageUrl)
+    {
+        this.imageUrl = imageUrl;
+    }
 }
