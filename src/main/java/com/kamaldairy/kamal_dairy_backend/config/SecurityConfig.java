b3f52e009f72a37f28d1b965d7cdf460e6dc9bc0
Jayesh <jayeshdhamal327@gmail.com>
2026-01-31 13:54:59 +0530

    User model created,hiberanate automatically created users table

--- src/main/java/com/kamaldairy/kamal_dairy_backend/config/SecurityConfig.java
+++ src/main/java/com/kamaldairy/kamal_dairy_backend/config/SecurityConfig.java
@@ -3,10 +3,20 @@ package com.kamaldairy.kamal_dairy_backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    // we have to make password encoder (for hashing the password)
+    @Bean
+    public PasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
+
+    // security rules basically who can access what, who will be blocked / who will be not blocked
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable()).authorizeHttpRequests
