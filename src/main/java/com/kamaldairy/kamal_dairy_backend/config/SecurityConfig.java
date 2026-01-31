2f61351e30fb7dd08264d2ac27a189532a230455
Jayesh <jayeshdhamal327@gmail.com>
2026-01-31 13:29:31 +0530

    Config package created (it will allow whom to allow who to block)

---
+++ src/main/java/com/kamaldairy/kamal_dairy_backend/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.kamaldairy.kamal_dairy_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable()).authorizeHttpRequests
+                (auth -> auth.requestMatchers
+                                ("/api/products/**" , "/api/trending-products/**", "/auth/**").permitAll().anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}
